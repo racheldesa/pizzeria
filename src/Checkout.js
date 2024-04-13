6ed52d5d8c891eb4d90c621ae6b8cc0443e7bfa1
racheldesa <racheldesa@outlook.com>
2024-04-13 13:32:30 -0500

    Add file - Checkout.js - Order payment information form

---
+++ src/Checkout.js
@@ -0,0 +1,11 @@
+/**
+ * Order payment information form
+ */
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.css";
+
+function Checkout () {
+    
+}
